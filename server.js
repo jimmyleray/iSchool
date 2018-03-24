223697c0fb3b10f9e48805a8346914fb065757c0
JimmyLERAY <jimmy.leray@protonmail.com>
2018-03-24 23:14:36 +0100

    typescript build

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const app = express()
 
 app.set('port', process.env.PORT || 1234)
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/build'))
 
 app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/public/dist/index.html')
+  res.sendFile(__dirname + '/build/index.html')
 })
 
 app.listen(app.get('port'), function() {
-	console.log('Node app is running on port', app.get('port'))
+  console.log('Node app is running on port', app.get('port'))
 })
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
 
 ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById('root'))
