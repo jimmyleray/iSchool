cd89be5b2979d717ed675a521f42fa38ddc3d9ad
JimmyLERAY <jimmy.leray@protonmail.com>
2018-03-24 20:39:39 +0100

    dev npm script

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.set('port', process.env.PORT || 5000)
+app.set('port', process.env.PORT || 1234)
 
 app.use(express.static(__dirname + '/public'))
 
