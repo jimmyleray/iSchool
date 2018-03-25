b011bab03f30fb15f12f1a8c12a401637d098ee6
JimmyLERAY <jimmy.leray@protonmail.com>
2018-03-25 23:20:33 +0200

    add github button

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react'
 import AppBar from 'material-ui/AppBar'
+import Button from 'material-ui/Button'
 import Toolbar from 'material-ui/Toolbar'
 import Typography from 'material-ui/Typography'
 import { withStyles, WithStyles } from 'material-ui/styles'
 
-const styles = { root: { flexGrow: 1 } }
+const styles = { root: { flexGrow: 1 }, flex: { flex: 1 } }
 
 export default withStyles(styles)(
-  class extends React.PureComponent<WithStyles<'root'>, {}> {
+  class extends React.PureComponent<WithStyles<'root' | 'flex'>, {}> {
     constructor(props) {
       super(props)
     }
@@ -17,7 +18,12 @@ export default withStyles(styles)(
         <div className={this.props.classes.root}>
           <AppBar position="static" color="default">
             <Toolbar>
-              <Typography variant="title">iSchool</Typography>
+              <Typography variant="title" className={this.props.classes.flex}>
+                iSchool
+              </Typography>
+              <Button href="https://github.com/JimmyLERAY/iSchool" color="primary">
+                GitHub
+              </Button>
             </Toolbar>
           </AppBar>
         </div>
