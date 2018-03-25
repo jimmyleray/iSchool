3e24e9c506e226573de86d371cc9b090cc1cb6aa
JimmyLERAY <jimmy.leray@protonmail.com>
2018-03-25 11:25:58 +0200

    rename app id

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
-ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById('root'))
+ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById('app'))
