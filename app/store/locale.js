3a07df24d47f25cd3ab89c2c00f1abfaa1c1e81f
JimmyLERAY <jimmy.leray@protonmail.com>
2018-03-27 00:05:57 +0200

    english default locale

--- app/store/locale.js
+++ app/store/locale.js
@@ -1,6 +1,6 @@
 export const changeLocale = locale => ({ type: 'CHANGE_LOCALE', locale })
 
-export default (state = navigator.language, action) => {
+export default (state = navigator.language || 'en', action) => {
   switch (action.type) {
     case 'CHANGE_LOCALE':
       return action.locale
