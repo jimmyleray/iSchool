ffc4f20792d712df89aa4a7c4d6c06608ac5b703
JimmyLERAY <jimmy.leray@protonmail.com>
2018-03-26 14:48:11 +0200

    page component

--- app/layouts/main.tsx
+++ app/layouts/main.tsx
@@ -2,17 +2,20 @@ import * as React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import routes from '../config/routes'
 import Navbar from '../components/navbar'
+import Page from '../components/page'
 
 export default () => (
   <React.Fragment>
     <Navbar />
-    {routes.map(route => (
-      <Route
-        key={route.path}
-        exact={route.exact || false}
-        path={route.path}
-        component={route.component}
-      />
-    ))}
+    <Page>
+      {routes.map(route => (
+        <Route
+          key={route.path}
+          exact={route.exact || false}
+          path={route.path}
+          component={route.component}
+        />
+      ))}
+    </Page>
   </React.Fragment>
 )
