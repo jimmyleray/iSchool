4cae9ffe3c856f7e038d55b487f5d5bcd5bba7c0
JimmyLERAY <jimmy.leray@protonmail.com>
2018-03-31 11:18:33 +0200

    loading is  pure component

--- app/components/loading.js
+++ app/components/loading.js
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { CircularProgress } from 'material-ui/Progress'
 
-export default class extends React.Component {
+export default class extends React.PureComponent {
   render() {
     return (
       <div style={{ padding: '10px' }}>
