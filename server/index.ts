916ac5b7b0fab4df996dc76a7245be945c9239b2
JimmyLERAY <jimmy.leray@protonmail.com>
2018-03-31 14:06:09 +0200

    mongodb local integration

--- server/index.ts
+++ server/index.ts
@@ -1,28 +1,46 @@
+// Import npm packages
 import * as compression from 'compression'
+import * as mongoose from 'mongoose'
 import * as express from 'express'
 import * as path from 'path'
-import { log, success } from './src/log'
 
-import * as mongoose from 'mongoose'
-mongoose.connect(process.env.MONGODB_URI).then(log)
+// Import logger utils
+import { log, success, error } from './src/log'
 
+// Connection to MongoDB
+mongoose
+  .connect(process.env.MONGODB_URI || 'mongodb://localhost:27017')
+  .then(() => log(success('Connected to MongoDB')))
+  .catch(() => log(error('MongoDB connection failed')))
+
+const Cat = mongoose.model('Cat', { name: String })
+const kitty = new Cat({ name: 'Zildjian' })
+kitty.save()
+
+// Create Express App
 const app = express()
 
+// GZip Compression
 app.use(compression())
 
+// Listening port set
 app.set('port', process.env.PORT || 4200)
 
+// Static directories declaration
 app.use(express.static(path.join(__dirname, 'client')))
 app.use(express.static(path.join(__dirname, 'public')))
 
+// Get locales json
 app.get('/locale/:lang', (req, res) => {
   res.sendFile(path.join(__dirname, 'locales', `${req.params.lang}.json`))
 })
 
+// All app routes
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'index.html'))
 })
 
+// Application start
 app.listen(app.get('port'), () => {
   log(success(`Server is running on port ${app.get('port')}`))
 })
--- server/src/log.ts
+++ server/src/log.ts
@@ -1,4 +1,5 @@
 import chalk from 'chalk'
 
 export const log = console.log
+export const error = chalk.redBright.bold
 export const success = chalk.greenBright.bold
