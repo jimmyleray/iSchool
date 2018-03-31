7222f0359cad0dfd2532f8b2cb8b64a8cc4fe055
JimmyLERAY <jimmy.leray@protonmail.com>
2018-03-31 19:18:34 +0200

    info log style

--- server/src/log.ts
+++ server/src/log.ts
@@ -1,5 +1,6 @@
 import chalk from 'chalk'
 
 export const log = console.log
+export const info = chalk.blueBright.bold
 export const error = chalk.redBright.bold
 export const success = chalk.greenBright.bold
